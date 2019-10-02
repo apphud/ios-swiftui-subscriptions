e6b31a961e81340ef71f20568455136c55269133
ren6 <renat.kurbanov@gmail.com>
2019-10-02 14:53:49 +0300

    Fixes to work on latest Xcode 11.0

--- subscriptions/Constants.swift
+++ subscriptions/Constants.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-let subscription_1 = "SixthWeekly"
-let subscription_2 = "sixth_weekly_regular"
-let shared_secret = "41f22138637c46458ee387e276ad53e2"
+let subscription_1 = "YOUR_PRODUCT_ID"
+let subscription_2 = "YOUR_ANOTHER_PRODUCT_ID"
+let shared_secret = "YOUR_SHARED_SECRET"
+
 let terms_text = "Premium subscription is required to get access to all wallpapers. Regardless whether the subscription has free trial period or not it automatically renews with the price and duration given above unless it is canceled at least 24 hours before the end of the current period. Payment will be charged to your Apple ID account at the confirmation of purchase. Your account will be charged for renewal within 24 hours prior to the end of the current period. You can manage and cancel your subscriptions by going to your account settings on the App Store after purchase. Any unused portion of a free trial period, if offered, will be forfeited when the user purchases a subscription to that publication, where applicable."
--- subscriptions/ProductsStore.swift
+++ subscriptions/ProductsStore.swift
@@ -16,7 +16,7 @@ class ProductsStore : ObservableObject {
     static let shared = ProductsStore()
     
     @Published var products: [SKProduct] = []
-    @Published var anyString = "123"
+    @Published var anyString = "123" // little trick to force reload ContentView from PurchaseView by just changing any Published value
     
     func handleUpdateStore(){
         anyString = UUID().uuidString
